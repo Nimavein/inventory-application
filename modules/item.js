d18f023c8d5e4204ded6a936f71a9560392343e0
Nimavein <michalzygiel00@gmail.com>
2021-05-29 13:30:37 +0200

    item module and folders structure create

---
+++ modules/item.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema({
+  name: { type: String, required: true, minLength: 2, maxLength: 60 },
+  description: { type: String, required: true, maxLength: 200 },
+  brand: { type: Schema.Types.ObjectId, ref: "Brand", default: "Other" },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  price: { type: Number, required: true, min: 0 },
+  number_in_stock: { type: Number, required: true, min: 0 },
+});
+
+ItemSchema.virtual("url").get(function () {
+  return "/catalog/item" + this._id;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
